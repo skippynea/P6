8b2db3ab65cdba958627137eb582648e8dadb845
skippynea <53440878+skippynea@users.noreply.github.com>
2023-05-22 10:33:50 +1100

    adjust the data models schema "ModelsSauce"

--- models/sauce.js
+++ models/sauce.js
@@ -11,8 +11,8 @@ const sauceSchema = mongoose.Schema({
     heat: { type: Number, required: true },
     likes: { type: Number, required: true },
     dislikes: { type: Number, required: true },
-    usersLiked: { type: String, required: true },
-    usersDisliked: { type: String, required: true }
+    usersLiked: [String],
+    usersDisliked: [String]
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
